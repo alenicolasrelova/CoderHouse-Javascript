9d98ed51ef77e82b29c289aaf3a131ad2a43c31b
AlejandroMio <alenicolasrelova@gmail.com>
2021-12-19 17:59:36 -0300

    Faltan ultimos proyectos

---
+++ Practica 11 - jQuery y Selectores/main.js
@@ -0,0 +1,26 @@
+/*let paises = $("#listaPais");
+console.log("Primer ejemplo");
+console.log(paises);
+console.log(typeof paises);
+
+let paises2 = $(".paises");
+console.log("Segundo ejemplo");
+console.log(paises2);
+console.log( typeof paises2 );
+
+let links = $("li");
+console.log(links[2].innerHTML);
+console.log(typeof links);
+
+
+let contenedores = $("div"); //Seleccion de todos los div
+console.log("Ejemplo con DIVS");
+console.log(contenedores);
+console.log(typeof contenedores);
+*/
+
+let enlaces = $("a:last");
+console.log("Ejemplo Enlaces");
+console.log(enlaces);
+console.log(typeof enlaces);
+
---
+++ Practica 12 - jQuery Selectores y eventos/js.js
@@ -0,0 +1,18 @@
+/*window.addEventListener("DOMContentLoaded"), function () {
+    console.log('El DOM esta listo');
+});*/
+
+$(document).ready(function){
+    console.log("El DOM esta listo (jquery)");
+    let mensaje = "Curso de <strong> Javascript</strong>"
+    $(#app).html(mensaje);
+}
+
+function mostrarMensaje(){
+    let mensaje="Curso de <strong> Javascript</strong>";
+
+}
+
+$(() => console.log("El dom esta OK")
+);
+mostrarMensaje();
